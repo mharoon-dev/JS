9794e9fd1a0f9ff30ef7d3f0aa8fbe8554430355
muhammad haroon <haroon.aghani@gmail.com>
2023-08-17 20:06:05 +0500

    basic JS

---
+++ class 01/js 1.js
@@ -0,0 +1,163 @@
+// number ke ilawa value string 
+// or 
+// ye inverted comma ke andar ho ga.
+
+// camelCase:2 words wale letter me second word ka first letter capital ho ga.
+
+// var firstName = "Muhammad"
+
+// var lastName =  "Haroon"
+
+// var address = "main street"
+// address = "frist street"
+
+// console.log(firstName, lastName,address );  
+// alert(firstName+" "+lastName+" "+address);
+// document.write(firstName+" "+lastName+" "+address)
+
+
+// let course = "html"
+// course = "css"
+
+
+// var firstName;
+// var lastName;
+// var address = "main street";
+
+// var firstName = "muhammad"
+
+// var lastName = "haroon"
+
+// var address = "first name"
+
+
+// chapter 01
+
+// alert("welcome to JS land.....");
+// alert("welcome to our website! we are glad you are here.");
+// alert("Error!Please enter a valid code.");
+// alert("Happy Coding!");
+
+// chapter 01 end
+
+
+
+// chapter 02
+
+
+// // 01
+// var myName =("Muhammad Haroon<br>");
+// document.write(myName);
+
+
+// // 02
+// var message =("Hello World");
+// alert(message);
+
+
+// // 03
+// var studentName =("Muhammad Haroon");
+// var age =("17 years old");
+// var profession =("UI Developer.");
+
+// alert(studentName);
+// alert(age);
+// alert(profession);
+
+// // 04
+// var multilineText = ("PIZZA \nPIZZ \nPIZ \nPI \nP");
+// alert(multilineText); 
+
+// // 05
+// var email = ("This is my email address 'haroon.aghani@gmail.com'.");
+// alert(email);
+
+// // 06
+// var book =("A smarter way to learn JavaScript.");
+// alert(book);
+
+// // 07
+// var text = ("Yah! I can write HTML content through JavaScript.");
+// document.write(text);
+
+// // 08
+// var string =("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+// alert(string);
+// d
+// // chapter 02 end
+
+
+
+// // chapter 03
+
+// // 01
+
+// var age =("I am 17 years old.");
+// alert(age);
+
+
+
+// // 02
+
+// var numberOfVisitingWebsite = ("You have visited this website 15 Time.");
+// alert(numberOfVisitingWebsite);
+
+// var numberOfVisitingWebsite = ("You have visited this website 16 Time.");
+// alert(numberOfVisitingWebsite);
+
+
+
+// // 03
+// var birthYear = ("My Birth-year is 2006.<br>");
+// document.write(birthYear);
+
+
+// // 04
+// var visitorName = ("hammad");
+// var productTitle = ("T-shirt");
+// var quantity = (150);
+
+// document.write("Hammad ordered 150 T-shirts on daraz.");
+
+
+
+// chapter 04
+
+// 01
+
+var heading = ("Rules for naming JS variables<br><br>");
+
+var point1 = ("Variable names can only contain numbers,_ and $. For example $my_1stVariable.<br>");
+var point2 = ("Variables must begin with a letter,_ or $. For example $name, _name or name.<br>");
+var point3 = ("Variable names are case sensitive.<br>");
+var point4 = ("Variable names should not be JS key-words.<br><br>");
+
+document.write(point1+" "+point2+" "+point3+" "+point4);
+
+
+
+// 02
+
+// legal var names
+
+document.write("legal names:<br>");
+var legalNames = ("point1, quantity, selling_product, $visitor, name<br><br>");
+document.write(legalNames);
+
+// legal var names end
+
+
+
+// illegal var names
+
+document.write("illegal names:<br>");
+var illegalNames = ("1point, @quantity, selling-Product, visitor!, name*");
+document.write(legalNames);
+
+// illegal var names end
+
+
+
+
+
+
---
+++ class 02/js 2.js
@@ -0,0 +1,218 @@
+// // // task 01
+
+
+// // 01
+
+// var street = "street#1";
+// var country = "pakistan";
+
+// var fullMailingAddress = street+" "+country;
+
+// console.log(fullMailingAddress);
+
+
+// // var number = 5;
+
+// // number +=10;
+// // number++  1 number add kare ga. agar pehle ho ga (++number) to add kar de ga.
+// // number--  1 number subtract kare ga. baad me ho to baad me add kare ga.
+
+
+// // console.log (number++);
+// // document.write(number);
+
+// // (%) modulus
+
+// // task 02
+
+// // 01
+// var score1 = 20;
+// var score2 = 30;
+// var score3 = 50;
+// var totalScore = score1 + score2 + score3;
+// var avearage = totalScore/3;
+// console.log(avearage)
+
+
+// // 02
+
+// var plates = 20;
+// var people = 10;
+// var remainingPlates = plates % people;
+// console.log(remainingPlates);
+
+// ++remainingPlates
+// console.log("There are 14 plates avalibles.");
+
+// // task 02 end
+
+
+
+// // home task
+
+// // chapter 05
+
+// // 01
+
+// // add
+
+// let add1 = (22);
+// let add2 = (44);
+
+// let add = add1 + add2;
+
+// document.write("The sum of 22 and 44 is 66.<br>");
+
+// // subtract
+
+// let sub1 = (44);
+// let sub2 = (22);
+
+// let subtract = sub1 - sub2;
+
+// document.write("The answer of 44-22 is 22.<br>");
+
+// // multiply
+
+// let multi1 = (20);
+// let multi2 = (10);
+
+// let multi = multi1 * multi2;
+
+// document.write("The asnwer of 20 * 10 is 200.<br>");
+
+// // division 
+
+// let division1 = 50;
+// let division2 = 10;
+
+// let division = division1 / division2;
+
+// document.write("The answer of 50 / 10 is 5.<br>" );
+
+// // modulus
+
+// let mod1 = 20;
+// let mod2 = 8;
+
+// let modules = mod1 % mod2;
+
+// document.write("The answer of 20 % 8 is 4.<br><br><br><br>");
+
+// // 02
+
+// let value 
+// value = 50
+// value++
+// value += 7 ;
+// value--
+// value /= 3;
+
+// document.write("The actual value is 50. <br>add 1 by using (++) <br>add 7 <br> subtract 1 by using (--) <br> value divide by 3 <br>answer is 19.<br><br><br><br> ")
+
+
+
+// // 03
+
+// let TicketPrice
+// TicketPrice = 600;
+// let total_ticket_price = (TicketPrice * 5);
+
+// document.write('The total amount of tickets id 3000.<br><br><br><br>');
+
+// // 04
+
+// document.write("TABLE OF 5 <br><br> 5x1=5<br>5x2=10<br>5x3=15<br>5x4=20<br>5x5=25<br>5x6=30<br>5x7=35<br>5x8=40<br>5x9=45<br>5x10=50<br><br><br><br><br>");
+
+
+// // 05
+
+// let priceOfItem1 = 10;
+// let priceOfItem2 = 20;
+// let orderedQuantityOfItem1 = 10;
+// let orderedQuantityOfItem2 = 5;
+// let shippingCharges = 10;
+
+// let totalCost = priceOfItem1 * orderedQuantityOfItem1 + priceOfItem2 * orderedQuantityOfItem2 + shippingCharges;
+// document.write("Receipt<br>");
+// document.write("Ordered Quantity of Item 1:10<br>");
+// document.write("Price of Item 1:10<br>");
+// document.write("Ordered Quantity of Item 2:5<br>");
+// document.write("Price of Item 2:20<br>");
+// document.write("Shipping Charges:10<br>");
+// document.write("Total Cost:210<br><br><br><br><br>");
+
+// // 06
+
+// const totalMarks = 700;
+// const obtained = 580;
+// const percent = 580 / 700 * 100;
+
+// document.write("total marks:"+ totalMarks+"<br>");
+// document.write("obtained:"+ obtained+"<br>");
+// document.write("percent:"+ percent+"<br><br><br><br>");
+
+
+// // 07
+
+// const USDollar = 1;
+// const pak_US =  104.80;
+// const we_Have_Dollar = 10;
+// const US_Calculation = we_Have_Dollar * pak_US;
+
+// document.write("US dollar = " + USDollar + "<br>");
+// document.write("1 Dollar in Pakistan = " + pak_US + "<br>");
+// document.write("We have US Dollar = "+ we_Have_Dollar + "<br>"); 
+// document.write("US 10 Dollar equal to = " + US_Calculation + " rupees in Pakistan.<br><br><br><br>" );
+
+// const Saudi    = 1;
+// const pak_Saudi = 28;
+// const we_Have_saudi = 25;
+// const Saudi_Calculation = we_Have_saudi * pak_Saudi;
+
+// document.write("Saudi = " + Saudi  + "<br>");
+// document.write("1 Riyall in Pakistan = " + pak_Saudi + "<br>");
+// document.write("We have Riyall = "+ we_Have_saudi + "<br>"); 
+// document.write("US 25 Riyall equal to = " + Saudi_Calculation + " rupees in Pakistan.<br><br>");
+
+// document.write("Total aomunt is " + (Saudi_Calculation + US_Calculation + "<br><br><br><br><br><br>"));
+
+
+
+// // 08
+
+// let number 
+// number = 50;
+// let calculation = (number + 5) * 10 / 2 ;
+// let Result = calculation;
+
+// document.write("number is 50<br>" +"calculation: "+ "(number + 5) * 10 / 2<br>" + "result: " + calculation + "<br><br><br><br><br>");
+
+
+// // 09
+
+// const currentYear = 2023;
+// const birthYear = 2006;
+
+// const age = (currentYear - birthYear);
+
+// document.write("current-year: " + currentYear + "<br>");
+// document.write("birth-year:   " + birthYear   + "<br>");
+// document.write("age: " + (currentYear - birthYear));
+
+
+// // 10
+
+
+// chapter 06
+
+
+
+// 01
+
+// var value = 20;
+// documnet.write("number:"+" "+ value);
+// number++
+// documnet.write("after this (number++) value is " + number);
+
+
