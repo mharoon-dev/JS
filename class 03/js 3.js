080dd03bec9442ee30bac803e3db064684f82fe0
muhammad haroon <haroon.aghani@gmail.com>
2023-08-18 22:08:11 +0500

    array

--- class 03/js 3.js
+++ class 03/js 3.js
@@ -1,13 +1,30 @@
-let bakery = ["makhan","paapay","doodh","plain cake","chips"];
+// let bakery = ["makhan","paapay","Doodh","plain cake","chips"];
 
-const itemNumber = +prompt("tell me the number");
+// const itemNumber = +prompt("tell me the number");
 
-alert("this is your product " + bakery[itemNumber] );
-
-
-const fruits = ["apple","mango","banana","orange","strawberry",]
+// alert("this is your product " + bakery[itemNumber] );
 
 
+
+
+
+
+                //01     //02    //03    //04      //05
+let fruits = ["apple","mango","banana","orange","strawberry",]
+
+//   and me koi element ka izafa karne ke liye
+
+                           //06
+fruits[fruits.length] = "kuch bhi" 
+console.log(fruits);
+
+
+fruits[fruits.length + 2] = "anannas" 
+console.log(fruits);
+
+
+fruits[fruits.length - 5] = "aam" 
+console.log(fruits);
 
 
 
