91eacef305b2d63ecbbe12a83c105f647498cccc
muhammad haroon <haroon.aghani@gmail.com>
2023-08-18 20:22:44 +0500

    prompt

--- class 02/js 2.js
+++ class 02/js 2.js
@@ -204,15 +204,98 @@
 // // 10
 
 
-// chapter 06
+    // chapter 06
 
 
 
-// 01
+    // 01
 
-// var value = 20;
-// documnet.write("number:"+" "+ value);
-// number++
-// documnet.write("after this (number++) value is " + number);
+    var d = 20;
+    document.write("The value of d is"+" "+ d+"<br><br>");
+
+    ++d
+    document.write("++d<br>");
+    document.write("Now the value of d is"+" "+ d+"<br><br>");
+
+    d++
+    document.write("d++<br>");
+    document.write("Now the value of d is"+" "+ d +"<br><br>");
+
+    --d
+    document.write("--d<br>");
+    document.write("Now the value of d is"+" "+ d+"<br><br>");
+
+    d--
+    document.write("d--<br>");
+    document.write("Now the value of d is"+" "+ d+"<br><br><br><br>");
+
+
+
+    // 02
+
+    let a = 2;
+    let b = 1;
+
+    let calculation = (--a - --b + ++b + b--);
+
+    document.write("a = 2<br>");
+    document.write("b = 1<br>");
+    document.write("calculation:--a - --b + ++b + b--<br>");
+    document.write("answer:1<br><br><br><br><br>");
+
+
+
+    // 03
+
+    let name = prompt("Write your name here...");
+    document.write("welcome,"+name+ " to our website<br><br><br><br><br>");
+
+    let number = prompt("which table you want");
+
+    document.write(number +"   x 1 =  "+ (number*1) +"<br>");
+    document.write(number +"   x 2 =  "+ (number*2) +"<br>");
+    document.write(number +"   x 3 = " + (number*3)  +"<br>");
+    document.write(number +"   x 4 = " + (number*4)  +"<br>");
+    document.write(number +"   x 5 = " + (number*5)  +"<br>");
+    document.write(number +"   x 6 = " + (number*6)  +"<br>");
+    document.write(number +"   x 7 = " + (number*7)  +"<br>");
+    document.write(number +"   x 8 = " + (number*8)  +"<br>");
+    document.write(number +"   x 9 = " + (number*9)  +"<br>");
+    document.write(number +"  x 10  ="+  (number*10)+"<br><br><br><br><br>");
+
+
+// 04
+
+let subject1 = prompt("subject no:01");
+let subject2 = prompt("subject no:02");
+let subject3 = prompt("subject no:03");
+
+let totalMarks = 100;
+
+let marks1 = prompt("marks no:01");
+let marks2 = prompt("marks no:02");
+let marks3 = prompt("marks no:03");
+
+let percent1 = ((marks1 / totalMarks) * 100);
+let percent2 = ((marks2 / totalMarks) * 100);
+let percent3 = ((marks3 / totalMarks) * 100);
+
+
+
+document.write(subject1    +" "+ totalMarks+" "+    marks1+" "+ percent1+"%<br>");
+document.write(subject2    +" "+ totalMarks+" "+    marks2+" "+ percent2+"%<br>");
+document.write(subject3    +" "+ totalMarks+" "+    marks3+" "+ percent3+"%<br>");
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- class 03/js 3.js
+++ class 03/js 3.js
@@ -1,21 +1,3 @@
-let name = prompt ("write your name here.");
-document.write ("welcome, " +name+ " to our website.<br>");
-
-
-let number = prompt("which table you want?");
-
-document.write(number +"   x 1 =  "+ (number*1) +"<br>");
-document.write(number +"   x 2 =  "+ (number*2) +"<br>");
-document.write(number +"   x 3 = " + (number*3)  +"<br>");
-document.write(number +"   x 4 = " + (number*4)  +"<br>");
-document.write(number +"   x 5 = " + (number*5)  +"<br>");
-document.write(number +"   x 6 = " + (number*6)  +"<br>");
-document.write(number +"   x 7 = " + (number*7)  +"<br>");
-document.write(number +"   x 8 = " + (number*8)  +"<br>");
-document.write(number +"   x 9 = " + (number*9)  +"<br>");
-document.write(number +"  x 10  ="+  (number*10)+"<br>");
-
-
 let bakery = ["makhan","paapay","doodh","plain cake","chips"];
 
 const itemNumber = +prompt("tell me the number");
