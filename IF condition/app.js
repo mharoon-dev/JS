42a5bfe8b3f6f6109be6b31c42a1f3612d503b8b
muhammad haroon <haroon.aghani@gmail.com>
2023-08-24 18:29:03 +0500

    if & else

--- IF condition/app.js
+++ IF condition/app.js
@@ -1,26 +1,38 @@
+// quiz
+
+// question01
+
 let ans1 = prompt ("write the full-form of HTML?");
-const point = 1;
+let point = 0;
 
 if (ans1.toLowerCase() === "hyper text markup language") {
-    alert("correct!you got "+ point + " point.");
+    alert("correct! you got 1 point.");
+    ++point;
+
 } else {
-    alert("sorry!you are wrong")
+    alert("sorry! you are wrong");
 }
 
+// question 02
 
 let ans2 = prompt ("write the full-form of JS?");
 
 if (ans2.toLowerCase() === "java script") {
-    alert("correct!you got "+ point +  " point.");
+    alert("correct! you got 1 point.");
+    ++point
 } else {
     alert("sorry!you are wrong");
-    document.write(point * 1)
 }
+
+// question 03
 
 let ans3 = prompt ("write the full form of CSS");
 
-if (ans3 === "Cascading Style Sheets") {
-    alert("correct!you got "+ point +  " point.")
+if (ans3.toLowerCase() === "cascading style sheets") {
+    alert("correct! you got 1 point.");
+    ++point
 } else {
-    alert("sorry!you are wrong")
+    alert("sorry! you are wrong")
 }
+
+document.write("you got " + point + " points.");
