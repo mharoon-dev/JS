43f9d4d4cb62d3fd0393ef4934725389875d9fd2
haroon124 <agar3d@gmail.com>
2023-09-05 15:07:04 +0500

    form

--- form/app.js
+++ form/app.js
@@ -1,28 +1,68 @@
-// calling all input fields
 let fName = document.getElementById("fname")
-let lname = document.getElementById("last name")
+let lName = document.getElementById("lname")
 let email = document.getElementById("email")
 let password = document.getElementById("password")
 let conformPassword = document.getElementById("conform password")
 
-const userName = "haroon.aghani@gmail.com" 
+// IDS
+const user1 = "haroon.aghani@gmail.com" 
+const user1P = "uideveloper"
+
+const user2 = "agar3d@gmail.com" 
+const user2P = "designer"
+
+const user3 = "kamil123@gmail.com" 
+const user3P = "frontenddeveloper"
+
+const user4 = "samad123@gmail.com" 
+const user4P = "jsdeveloper"
 
 function signupButton() {
-    if (fName.value == "" ||
-        lname.value == "" ||
-        email.value == "" ||
-        password.value == "" ||
-        conformPassword.value == "") {
-        alert("fill all the fields")
-        } else if (
-            password != conformPassword
-        ) {
-            alert("password and conform-password are not matching.")
-        } else if (
-            email == userName
-        ) {
-            alert("your account is already exist.\nplease,go to the login page.")
-        } else {
-            alert("User signup successfully\nWelcome to our website.")
+
+    // checking fname values
+    if (fName.value == "") {
+        alert("Your first name is missing.")
         }
-}
+
+    // checking lname values
+        if (lName.value == "") {
+            alert("Your last name is missing.")
+    } 
+
+    // checking email values
+    if (email.value == "") {
+        alert("Your email is missing.")
+    }
+
+    // checking password values
+    if (password.value == "") {
+        alert("Your password is missing.")
+    }
+
+    // checking conform password values
+    if (conformPassword.value == "") {
+    alert("Your conform-Password is missing.")
+    }
+
+    // checking password and conform password is equal or not
+    if (password.value != conformPassword.value) {
+        alert("password and conform-password are not matching.")
+    }
+
+    // checking is account already exist?
+    if (email.value == user1) {
+        alert("Account is already exist from this email address.")
+    } else if (email.value == user2) {
+        alert("Account is already exist from this email address.")
+    } else if (email.value == user3) {
+        alert("Account is already exist from this email address.")
+    } else if (email.value == user4) {
+        alert("Account is already exist from this email address.")
+    } else {
+        alert("user sign-up successfully")
+    } 
+    window.location = './home.html'
+} 
+
+
+
